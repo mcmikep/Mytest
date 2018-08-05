e182902a8ba9d145599937fc90348379a89e2047
mcmikep <mcmikep@gmail.com>
2018-08-05 08:51:19 -0500

    first commit

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<HEAD>
+	<link href="index.css" rel="stylesheet">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="stylesheet" href="animate.css">
+</HEAD>
+<?php
+
+	echo file_get_contents("FireKitchen.svg");
+
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src ="script.js">
+
+</script>
